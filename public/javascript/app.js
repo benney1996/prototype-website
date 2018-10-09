d754eb9aa8f5a86d9b1a8f0a8fba24667a46608e
oscar <oscar@zealley.com>
2018-10-09 10:04:48 -0400

    BEfore Heroku

---
+++ public/javascript/app.js
@@ -0,0 +1,23 @@
+/*global $ */
+$(document).ready(function(){
+  
+  var tabs = ['contact', 'about', 'prototype'];
+
+  function eventHandler(event){
+    event.preventDefault();
+    tabs.forEach(function(item){
+      if(event.currentTarget.id === item){
+        $('#' + item).addClass('active');
+        $('.'+item).removeClass('hidden');
+      } else {
+        $('#' + item).removeClass('active');
+        $('.' + item).addClass('hidden');
+      }
+    });
+  }
+
+  $('#prototype').click(eventHandler);
+  $('#about').click(eventHandler);
+  $('#contact').click(eventHandler);
+
+});
